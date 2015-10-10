3dbd8dc92cb45e81fa302cfcb9d316a06fd918d5
edgenard / kegreen22 <edgenard@gmail.com , kegreen22@gmail.com>
2015-10-10 16:13:37 +0000

    Cannot get testing to work with Grape

---
+++ spec/api/email_listicle_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "email_listicle_api" do
+   describe "GET /all" do
+       it "returns all undecided links" do
+           #setup
+            undecided_email = double("email")
+            allow(EmailLink).to receive(:undecided).and_return(undecided_email)
+           #exercise
+           get "/all", {}, { "Accept" => "application/json" }
+           
+           #verifies
+           expect(response).to be(undecided_email)
+       end
+       
+       
+       
+   end
+    
+    
+    
+    
+    
+end
