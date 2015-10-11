c6fea88fa0d7267ccd499969aa552912811b6239
Bryce McDonnell <bryce@bridgetownint.com>
2015-10-11 16:25:06 -0400

    validates emaillink model factory

--- spec/models/emaillink_spec.rb
+++ spec/models/emaillink_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Emaillink, type: :model do
-    it "creates an e-mail link" do
-       Emaillink.create!(cnt_title_words:6, email_subject:'cray computer', 
-       from_email:'kegreen22@gmail.com', from_name:'K Green', 
-       url:'www.nytimes.com', title:'Cray invents new computer', user_id:1) 
-        
-        expect(Emaillink.count).to eq(1)
-    end
+  it 'default factory is valid' do
+    expect(build(:emaillink).valid?).to be_truthy
+  end
 end
