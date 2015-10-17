5c139e17d498a93028ebed01ce86e64106bd22d2
edgenard / kegreen22 <edgenard@gmail.com , kegreen22@gmail.com>
2015-10-17 10:45:04 -0400

    add ruby version and profile to gitignore

--- spec/models/email_link_spec.rb
+++ spec/models/email_link_spec.rb
@@ -2,29 +2,29 @@ require 'rails_helper'
 
 RSpec.describe EmailLink, type: :model do
     it "default factory is valid" do
-       expect(build(:email_link).valid?).to be_truthy 
-    
-  
+       expect(build(:email_link).valid?).to be_truthy
+
+
     end
-    
+
     it "fails validations with invalid attributes" do
         expect(build(:email_link, url:"").valid?).to be_falsy
     end
-    
+
     it "returns undecided emails" do
-    
+
         email1 = build(:email_link)
 
         email2 = build(:email_link, accept_or_rejected: Time.now, accepted: true)
-       
+
         result = EmailLink.undecided
-        
+
         expect(result).to include(email1)
         expect(result).not_to include(email2)
     end
 end
 
 
-#email1 = build(:email_link) 
+#email1 = build(:email_link)
 #email2 =  build(:email_link(accepted_or_rejected: Datetime.now, accepted: false)
 #expect(EmailLink.undecided).to include(email1)
