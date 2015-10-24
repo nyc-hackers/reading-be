f3f049b5cdcfb26c16f0e1d087f0d934166e451e
edgenard / kegreen22 <edgenard@gmail.com , kegreen22@gmail.com>
2015-10-24 12:25:21 -0400

    Working on getting marking email links as read to work, route is not showing up

--- app/api/email_link_resource.rb
+++ app/api/email_link_resource.rb
@@ -10,6 +10,15 @@ module Listicle
       get :unread do
         EmailLink.unread
       end
+
+      desc "marks email as read"
+      put :read do
+        email = EmailLink.find(params[:id])
+        email.read = true
+        email.save
+
+        {status: 200}
+      end
     end
   end
 end
