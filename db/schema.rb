13617f01cad4f237108e7bcf8774966a533ca32d
edgenard / kegreen22 <edgenard@gmail.com , kegreen22@gmail.com>
2015-10-31 11:21:32 -0400

    Ran rake db:migrate after change in schema

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20151012192529) do
     t.string   "title",                 null: false
     t.integer  "label_id"
     t.integer  "user_id"
-    t.boolean  "read"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
   end
