43ad9ef6f8025df96a1684c0480a3b5aae28e171
edgenard / kegree22 <edgenard@gmail.com , kegreen22@gmail.com>
2015-12-20 12:19:24 -0500

    Add failure action in CallbacksController. Comment out omniauth.rb

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,11 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    sign_in @user
+    redirect_to root_url
+  end
+
+  def failure
+    redirect_to root_url
   end
 end
