3526c1cc21184d6b69ad0fe77a573fa16ea709ba
edgenard / kegreen22 <edgenard@gmail.com , kegreen22@gmail.com>
2015-12-27 11:35:03 -0500

    added flash messages for succesful login and unsuccessful login

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,10 +2,12 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in @user
+    flash[:notice] = "Successful login"
     redirect_to root_url
   end
 
   def failure
+    flash[:notice] = "Login Unsuccessful"
     redirect_to root_url
   end
 end
